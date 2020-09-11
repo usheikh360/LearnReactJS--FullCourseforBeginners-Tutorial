988b543bd60989ffa7d9bd2cc9160852659c6c80
“Umair <ubsheikh.4@gmail.com>
2020-09-11 12:50:25 +0100

    Mved component into seprate file

--- myfirstapp/src/index.js
+++ myfirstapp/src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
-import MyInfo from "./MyInfo";
+import MyInfo from "./components/MyInfo";
 
 ReactDOM.render(
   <MyInfo />,
