5f29f65fc995f75d63e76ca0fd2b8611097f8632
“Umair <ubsheikh.4@gmail.com>
2020-09-11 12:40:20 +0100

    React Functional Components

--- myfirstapp/src/index.js
+++ myfirstapp/src/index.js
@@ -1,17 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+// import './index.css';
+// import App from './App';
+// import * as serviceWorker from './serviceWorker';
+
+
+function MyInfo() {
+  return (<div>
+    <h1>Umair Sheikh</h1>
+    <p>I love to code, enjoy movies and going out with freinds!</p>
+    <h3>Top 3 vacation spots I would love to visit:</h3>
+    <ol>
+      <li>Spain</li>
+      <li>Paris</li>
+      <li>Dubai</li>
+    </ol>
+  </div>)
+}
+
+
+
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <MyInfo />,
   document.getElementById('root')
 );
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// // If you want your app to work offline and load faster, you can change
+// // unregister() to register() below. Note this comes with some pitfalls.
+// // Learn more about service workers: https://bit.ly/CRA-PWA
+// serviceWorker.unregister();
