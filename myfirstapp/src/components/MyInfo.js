519270ff063f085a895c69991c8259ee1a7c4b55
“Umair <ubsheikh.4@gmail.com>
2020-09-11 12:49:26 +0100

    Mved component into seprate file

---
+++ myfirstapp/src/components/MyInfo.js
@@ -0,0 +1,16 @@
+import React from "react";
+
+function MyInfo() {
+    return (<div>
+        <h1>Umair Sheikh</h1>
+        <p>I love to code, enjoy movies and going out with freinds!</p>
+        <h3>Top 3 vacation spots I would love to visit:</h3>
+        <ol>
+            <li>Spain</li>
+            <li>Paris</li>
+            <li>Dubai</li>
+        </ol>
+    </div>)
+}
+
+export default MyInfo;
--- myfirstapp/src/index.js
+++ myfirstapp/src/index.js
@@ -3,23 +3,7 @@ import ReactDOM from 'react-dom';
 // import './index.css';
 // import App from './App';
 // import * as serviceWorker from './serviceWorker';
-
-
-function MyInfo() {
-  return (<div>
-    <h1>Umair Sheikh</h1>
-    <p>I love to code, enjoy movies and going out with freinds!</p>
-    <h3>Top 3 vacation spots I would love to visit:</h3>
-    <ol>
-      <li>Spain</li>
-      <li>Paris</li>
-      <li>Dubai</li>
-    </ol>
-  </div>)
-}
-
-
-
+import MyInfo from "./MyInfo";
 
 ReactDOM.render(
   <MyInfo />,
